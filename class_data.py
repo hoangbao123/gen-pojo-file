35c9149ed47bc35f3ed042efbc2baec7cf3c10f8
hoang nghia bao <baohn@tima.vn>
2020-11-16 17:58:33 +0700

    add gen object from jso

--- class_data.py
+++ class_data.py
@@ -3,35 +3,44 @@ class ClassData:
     def __init__(self, class_name):
         self.class_name = class_name
         self.class_attributes = []
+        self.inner_class = {}
     
     def gen_class_file(self, file_type='java'):
         if self.class_name is None or self.class_attributes is None:
             raise ValueError("Not enough data to gen class")
         else:
             if (file_type == 'java'):
-                self.gen_java_class()
+                f = open(f"generated_data/{self.class_name.capitalize()}.java", "w")
+                self.gen_java_class(f)
             else:
                 raise ValueError(f"File type {file_type} is not supported")
     
-    def gen_java_class(self):
-        with open(f"generated_data/{self.class_name.capitalize()}.java", "w") as f:
-            f.write(f"public class {self.class_name}" + "{ \n")
-            for class_attribute in self.class_attributes:
-                attr_name = class_attribute['name']
-                attr_type = class_attribute['data_type']
-                attr_gson = class_attribute['gson']
-                attr_jackson = class_attribute['jackson']
-                attr_column = class_attribute['column']
-                # attr_name_type = class_attribute['name_type']
-                if attr_gson is not None:
-                    f.write(self.IDENT +"@SerializedName(\"" + attr_gson + "\")"  + "\n")
-                if attr_jackson is not None:
-                    f.write(self.IDENT + "@JsonProperty(\"" + attr_jackson + "\")" + "\n")
-                if attr_column is not None:
-                    f.write(self.IDENT + "@Column(name = \"" + attr_column + "\")" + "\n")    
-                f.write(self.IDENT + f"private {self.mapping_type(attr_type)} {attr_name};" + "\n")
-                f.write("\n")
-            f.write("}")
+    def gen_java_class(self, fw, inner=False, num_indent=1):
+        if not inner:
+            fw.write(f"public class {self.class_name}" + "{ \n")
+        else:
+            fw.write(self.IDENT * (num_indent - 1) + f"public static class {self.class_name}" + "{ \n")
+
+        for class_attribute in self.class_attributes:
+            attr_name = class_attribute['name']
+            attr_type = class_attribute['data_type']
+            attr_gson = class_attribute['gson']
+            attr_jackson = class_attribute['jackson']
+            attr_column = class_attribute['column']
+            # attr_name_type = class_attribute['name_type']
+            if attr_gson is not None:
+                fw.write(self.IDENT * num_indent + "@SerializedName(\"" + attr_gson + "\")"  + "\n")
+            if attr_jackson is not None:
+                fw.write(self.IDENT * num_indent + "@JsonProperty(\"" + attr_jackson + "\")" + "\n")
+            if attr_column is not None:
+                fw.write(self.IDENT * num_indent + "@Column(name = \"" + attr_column + "\")" + "\n")    
+            fw.write(self.IDENT * num_indent + f"private {self.mapping_type(attr_type)} {attr_name};" + "\n")
+            fw.write("\n")
+
+        for k, v in self.inner_class:
+            v.gen_java_class(fw, True, num_indent + 1)
+            
+        fw.write(self.IDENT * (num_indent - 1) + "}")
 
 
     def mapping_type(self, source_type: str, program_language='java'):
@@ -51,5 +60,9 @@ class ClassData:
         elif source_type.lower() == "timestamp":
             des_type = "ZonedTimeDate"
         else:
-            raise ValueError(f"This type '{source_type}' in {program_language} is not supported")
+            if source_type in self.inner_class:
+                des_type = source_type 
+            else:
+                raise ValueError(f"This type '{source_type}' in {program_language} is not supported")
+
         return des_type
--- gen_pojo_object.py
+++ gen_pojo_object.py
@@ -1,5 +1,5 @@
 from class_data import ClassData
-
+import json 
 
 def gen_var_name(original_name):
     values = original_name.split("_")
@@ -10,6 +10,13 @@ def gen_var_name(original_name):
         for i in range(1, len(values)):
             class_name += values[i].capitalize()
         return class_name
+
+def gen_class_name(original_name):
+    values = original_name.split("_")
+    class_name = ""
+    for i in range(0, len(values)):
+        class_name += values[i].capitalize()
+    return class_name
 
 
 def gen_attr_dict(original_name, generated_name, data_type, column=True, gson=True, jackson=True):
@@ -65,7 +72,49 @@ def gen_pojo(source_file, source_type='db-diagram'):
                     attr_dict = gen_attr_dict(values[0], attr_name, attr_data, True, True, True)
                     current_class_attr.append(attr_dict)
             
-            
-                       
+
+def gen_pojo_from_object(source_object, class_name):
+    if isinstance(source_object, dict):
+        new_class = ClassData(class_name)
+        current_attr = []
+        inner_class_dict = {}
+        for k, v in source_object.items():
+            if isinstance(v, int):
+                current_attr.append(gen_attr_dict(k, gen_var_name(k), 'int', True, True, True))
+            elif isinstance(v, float):
+                current_attr.append(gen_attr_dict(k, gen_var_name(k), 'double', True, True, True))
+            elif isinstance(v, str):
+                current_attr.append(gen_attr_dict(k, gen_var_name(k), 'String', True, True, True))
+            elif isinstance(v, bool):
+                current_attr.append(gen_attr_dict(k, gen_var_name(k), 'Boolean', True, True, True))
+            elif isinstance(v, list):
+                current_attr.append(gen_attr_dict(k, gen_var_name(k), f'List<{gen_class_name(k)}>', True, True, True))
+                inner_class = gen_pojo_from_object(v[0], gen_class_name(k))
+                inner_class_dict[f'List<{gen_class_name(k)}>'] = inner_class
+            elif isinstance(v, dict):
+                current_attr.append(gen_attr_dict(k, gen_var_name(k), gen_class_name(k), True, True, True))
+                inner_class = gen_pojo_from_object(v, gen_class_name(k))
+                inner_class_dict[gen_class_name(k)] = inner_class
+        new_class.class_attributes = current_attr
+        new_class.inner_class = inner_class_dict
+        return new_class
+
+
+def gen_pojo_from_json(source_file, class_name="random"):
+    f = open(source_file)
+    data = json.load(f)
+    need_class = gen_pojo_from_object(data, class_name)
+    need_class.gen_class_file('java')
+    
+
+def gen_pojo_from_list(li):
+    pass 
+
+
+def gen_pojo_from_dict(li):
+    pass
+
+
 if __name__ == "__main__":
-   gen_pojo("/home/hoangbao/workspace/utils/raw_data/v1.txt")
+#    gen_pojo("/home/hoangbao/workspace/utils/raw_data/v1.txt")
+    gen_pojo_from_json("raw_data/data.json")
